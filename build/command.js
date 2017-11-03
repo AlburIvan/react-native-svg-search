d396bab0203d4c65a74ec9c080a87cd12ad26640
Ivan Alburquerque <Albur.Ivan@outlook.com>
2017-11-03 16:54:54 -0400

    Delete ts build examples

--- build/command.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _example = require("./example");
-
-var _example2 = _interopRequireDefault(_example);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
