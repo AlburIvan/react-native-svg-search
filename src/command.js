0d6576fe5769e6df05d4ceedeb65ddf67c8a49b5
AlburIvan <albur.ivan@outlook.com>
2017-11-06 02:37:15 -0400

    Modify class naming behavior, clean comments

--- src/command.js
+++ src/command.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
-import {
-    Command
-} from "commander";
-
-import {
-    prompt
-} from "inquirer";
-
+import { Command } from "commander";
+import { prompt } from "inquirer";
 import FlatIconParser from "./flaticon.parser.js";
+import Utils,{ getDefaultName, getClassName } from "./utils.js";
 
 
-// https://scotch.io/tutorials/build-an-interactive-command-line-application-with-nodejs
 const shell = new Command();
+
+let data = {
+    svgName: "",
+    fileName: "",
+    className: "",
+    outputDir: ""
+}
 
 const questions = [
     {
@@ -19,39 +20,81 @@ const questions = [
         name : 'svg',
         message : 'Whats the name of the Icon:'
     },
-    // {
-    //     type : 'input',
-    //     name : 'filename',
-    //     message : 'Whats name should we give the new SVG icon:'
-    // }
+    {
+        type : 'input',
+        name : 'filename',
+        message : 'What name should the new file have (we will append .icon.js):'
+    },
+    {
+        type : 'input',
+        name : 'output',
+        message : 'Where should we save the new file (default assets/icons):'
+    }
 ];
 
 
 shell
-    .version('0.0.1')
-    .description('This CLI gets an icon from FlatIcon and ouputs the react-native compatible SVG in a folder');
+    .version('1.0.0')
+    .description('This CLI gets an icon from FlatIcon and ouputs the react-native compatible SVG in a folder')
+    .option('-n, --name', 'The name of the SVG icon as seen in FlatIcon\'s url')
+    .option('-f, --filename <filename>', 'The new name we should give to the icon (we append .icon.js)')
+    .option('-o, --output <output>', 'Save the SVG ouput in a different directory');
 
-// https://www.flaticon.com/free-icon/looking_230295
-// https://image.flaticon.com/icons/svg/230/230295.svg
+
 shell
-    .command('search-svg [svg] [filename]')
-    .alias('')
-    .description('Search the given icon name at FlatIcon')
-    .action((svg, filename) => {
+    .command('search [name] [filename] [output]')
+    .description('Search the given name as an icon on FlatIcon')
+    .usage('-n database_148825 -f database -o assets/icons')
+    .option('-n, --name <name>', 'The name of the SVG icon as seen in FlatIcon\'s url')
+    .option('-f, --filename <filename>', 'The new name we should give to the icon (we append .icon.js)')
+    .option('-o, --output <output>', 'Save the SVG ouput in a different directory')
+    .action( (name, filename, output) => {
 
-        if(!svg) {
-            prompt(questions)
-            .then((answers) => {
+        if(name) {
 
-                let parser = new FlatIconParser();
+            if( output === undefined) {
+                console.log("Output dir not defined, using default one [assets/icons]");
+            }
 
-                parser.getReactIcon(answers);
-            });
-        } else {
+            if( filename === undefined) {
+                filename = getDefaultName(name);
+                console.log(`File's name not defined, using default one [${filename}]\n`);
+            }
+
+            data.svgName = name;
+            data.className = getClassName(filename);
+            data.fileName = getDefaultName(filename);
+            data.outputDir = output === undefined ? "assets/icons" : output;
+
             let parser = new FlatIconParser();
-            parser.getReactIcon(svg, true);
+            parser.getReactIcon(data);
         }
-       
+        else {
+            prompt(questions)
+                .then((answers) => {
+
+                    if(!answers.svg) {
+                        throw Error("No icon name defined");
+                    }
+
+                    if(!answers.output) {
+                        console.log("\nOutput directory not defined, using default one [assets/icons]\n");
+                    }
+        
+                    if(!answers.filename) {
+                        answers.filename = getDefaultName(answers.svg);
+                        console.log(`File's name not defined, using default one [${answers.filename}]\n`);
+                    }
+
+                    data.svgName = answers.svg;
+                    data.className = getClassName(answers.filename);
+                    data.fileName = getDefaultName(answers.filename);
+                    data.outputDir = answers.output ? answers.output : "assets/icons";
+
+                    let parser = new FlatIconParser();
+                    parser.getReactIcon(data);
+                });
+        }
     })
 
 shell.parse(process.argv);
--- src/flaticon.parser.js
+++ src/flaticon.parser.js
@@ -1,58 +1,246 @@
 
-import  * as fs from "fs";
-import * as axios from "axios";
+import fs from "fs";
+import axios from "axios";
+import mkdirp from "mkdirp";
+import debug from "debug";
 
+/**
+ * This is a specific parser for https://FlatIcon.com 's free icons, 
+ * the goal of this CLI is to provide a faster way to include
+ * SVG icons in your react-native project.
+ */
 export default class FlatIconParser {
 
-    constructor() {}
-
-    getReactIcon(answers) {
-        this._getdownloadURL(answers.svg);
+    /**
+     * Constructor used to initialize data object
+     */
+    constructor() {
+        this.data = null;
     }
 
-    getReactIcon(svgName, shouldGenerateName) {
+    /**
+     * Controls the flow of the CLI, all steps are pipelined from here
+     * 
+     * @param {Array} data 
+     */
+    getReactIcon(data) {
+
+        this.data = data;
         
-        let url = this._getdownloadURL(svgName);
+        let url = this._getdownloadURL(data.svgName);
         let iconXml = this._downloadIcon(url);
 
-        iconXml.then((resp) => {
+        iconXml
+            .then((resp) => {
 
-            if(resp === 500) {
-                console.log("SERVER");
-                return;
-            }
+                if(resp == 500) {
+                    throw Error("For some reason we couldn't reach flaticon.com's servers, try again later...");
+                }
 
-            if(resp === 404) {
-                console.log("NOT FOUND");
-                return;
-            }
+                if(resp == 404) {
+                    throw Error("Couldn't find the specified icon, either there was a mistake copying the name or the icon is premium, try again...");
+                }
 
-            let svgXml = resp.replace(/<g>(\r\n|\r|\n)<\/g>/g, '');
-            console.log(`${svgXml}`);
+                return resp;
+            })
+            .then((resp) => {
+                return this._parseXml(resp);
+            })
+            .then((clazz) => {
+                return this._createReactComponent(clazz);
+            })
+            .then((component) => {
+               return this._saveComponent(data, component);
+            })
+            .catch((err) => {
+                console.log(err.message);
+                console.log("\n\n[if you believe this is a mistake please open an issue @ https://github.com/AlburIvan/react-native-svg-search/issues]");
+            });
+    }
+
+
+
+    /**
+     * Hacky method that parses the SVG's XML and begins to sanitize and transform 
+     * all the inputs to be compatible with react-native-svg.
+     * 
+     * Also here we get all the tags that needs to be imported on the file
+     * 
+     * @param {String} xml 
+     */
+    _parseXml(xml) {
+
+        console.log('Transforming FlatIcon SVG input to react-native compatible SVG...\n');
+
+        // clean all the empty tags
+        let sanitizedXml = xml.replace(/<.>(\r\n|\r|\n)<\/.>/g, '');
+
+        // capitalize all the start/end tags
+        let capitalizedXml = sanitizedXml
+            .replace(/(?:<)(\w)/g, (x) => { 
+                return '<' + x[1].toUpperCase()
+            })
+            .replace(/(?:<)\/(\w)/g, (x) => { 
+                return '</' + x[2].toUpperCase()
+            });
+
+
+        let cleanedXml = capitalizedXml.split(/\r\n|\r|\n/)
+            .filter((line)=> {
+                return !(line.startsWith('<!') || line.startsWith('<?') || line === '');
+            })
+            .map((line, index) => {
+
+                // remove useless id attr
+                line = line.replace(/id="\w*"/g, '');
+
+                // remove useless 'style' attr and extract the inside one
+                line = line.replace(/style=\"(.*):(.*;)\"/g, '$1="$2"').replace('new','');
+
+                // replace semicolon on attrs
+                line = line.replace(';', '');
+
+                // replace xmlns:space
+                line = line.replace(/xml:space="preserve"/g,'')
+
+                // special case
+                if(line.includes("<Svg")) {
+
+                    line = line
+                        // replace xmlns
+                        .replace(/xmlns=".*g"/g,'')
+                        // replace xmlns:link
+                        .replace(/xmlns:xlink=".*k"/g, '')
+                        // replace x property with width prop
+                        .replace(/x="\w*."/g, 'width={this.props.width}')
+                        // replace y property with height prop
+                        .replace(/y="\w*."/g, 'height={this.props.height}')
+                        
+                }
+
+                return line;
+            })
+            .join('\n');
+
+        // get all the tags used
+        let tags = cleanedXml.match(/(?:<)(\w*)/g);
+
+        tags = tags.map((match) => {
+            return match.replace('<','');
         });
-     
+
+        let imports = [];
+
+        tags.forEach((element) => {
+            if(!element || element === 'Svg') return;
+
+            if(imports.indexOf(element) == -1) 
+                imports.push(element);
+        }, this);
+
+
+        let clazz = {
+            imports: imports,
+            svg: cleanedXml
+        }
+        
+        return clazz;
+    }
+
+    _createReactComponent(clazz) {
+
+        console.log("Creating React-Native component...\n");
+
+        let reactClass = 
+`
+import React, { Component } from "react";
+
+import Svg, { ${clazz.imports} } from "react-native-svg";
+
+export default class ${this.data.className} extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return(
+            ${clazz.svg}
+        );
+    }
+}`;
+        console.log("React-Native component created...\n");
+
+        return reactClass;
+    }
+
+
+    /**
+     * This method is in charge of saving the component in the specified output directory,
+     * if the directory doesn't exists it will create a new one.
+     * 
+     * @param {Array} data
+     * @param {ReactClass} component 
+     */
+    _saveComponent(data, component) {
+       
+        console.log(`Saving new component on ${data.outputDir}\n`);
+
+        this._dirExistsOrCreate(data.outputDir)
+            .then((message) => {
+
+                let path = `${data.outputDir}/${data.fileName}`
+
+                fs.writeFile(path, component, function(err) {
+                    if(err) {
+                        console.log(`[Failure] -> The icon couldn't be saved in [${data.outputDir}] because [${err.message}]]`);
+                    }
+                    else {
+                        console.log(`[Successful] -> The icon was saved in [${data.outputDir}] with name [${data.fileName}]`);
+                    }         
+                }); 
+            });
     }
    
-    // TODO: create a method to download the icon and then change all elements to react-native-svg elements
+
+
+    /**
+     * Handles the creation of directories.
+     * 
+     * @param {String} dirPath path
+     */
     _dirExistsOrCreate(dirPath) {
-        fs.mkdir(dirPath, (err) => {
-            if(err) {
-                console.log(`Could not create ${dirPath} directory: ${err}`);
-            }
-            else {
-                console.log(`mkdir ${dirPath} successful`);
-            }
+        return new Promise((resolve, reject) => {
+            mkdirp(dirPath, (err) => {
+                if(err) {
+
+                    if(err.code === 'EEXIST') 
+                        resolve(`${dirPath} already exists, ommiting...`);
+                    else
+                        reject(`Could not create ${dirPath} directory: ${err}`);
+                }
+                else {
+                    resolve(`mkdir ${dirPath} successful`);
+                }
+            });
         });
     }
-   
 
+
+    /**
+     * Downloads the SVG icon from the specified named endpoint
+     * 
+     * @param {String} url 
+     */
     _downloadIcon(url) {
+
+        console.log("Downloading SVG...\n");
+
         return axios.get(url)
             .then(response => {
 
-                if(response.status == 404) {
-                    console.log("NOT FOUND");
-                    return undefined;
+                if(response.status != 200) {
+                    return response.status;
                 }
 
                 return response.data;
@@ -62,16 +250,19 @@ export default class FlatIconParser {
             });
     }
 
+    /**
+     * Parses the SVG's name to create a download URL compliance with FlatIcon's image endpoint
+     * 
+     * @param {String} svgName 
+     */
     _getdownloadURL(svgName) {
 
-        let url = "https://image.flaticon.com/icons/svg/$id/$fullId.svg"; 
-
-        let id = svgName.substring(svgName.indexOf('_') + 1, 8);
+        let id = svgName.substring(svgName.indexOf('_') + 1, svgName.indexOf('_') + 4);
         let fullId = svgName.substring(svgName.indexOf('_') + 1);
 
-        url = url.replace("$id", id).replace("$fullId", fullId);
+        let url =  `https://image.flaticon.com/icons/svg/${id}/${fullId}.svg`;
 
-        console.log(`URL: ${url}`);
+        console.log(`\nYour download URL is: ${url}\n`);
 
         return url;
     }
